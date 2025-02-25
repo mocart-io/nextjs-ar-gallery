533303ea8f084051ca6466a3daf0ee3e982b1c59
Murad Abu-Gosh <muradabgosh@gmail.com>
2025-02-25 16:13:31 +0200

    Added hover-for-3d iFrames

---
+++ components/card-ad.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from "react";
+import { Box, Center } from "@chakra-ui/react";
+
+interface CardAdProps {
+    ad: {
+        url: string;
+    };
+}
+
+const CardAd: FC<CardAdProps> = ({ ad }) => {
+    return (
+        <Center py={12}>
+            <Box
+                role="group"
+                p={6}
+                border="1px solid rgba(197, 199, 208, 0.2)"
+                rounded="16px"
+                boxShadow="2xl"
+                pos="relative"
+                zIndex={1}
+            >
+                <Box rounded="lg" pos="relative" overflow="hidden">
+                    <iframe
+                        src={ad.url}
+                        width="100%"
+                        height="300px"
+                        style={{ border: 0 }}
+                        title="Ad content"
+                    />
+                </Box>
+            </Box>
+        </Center>
+    );
+};
+
+export default CardAd;
